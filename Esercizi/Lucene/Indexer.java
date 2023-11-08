b240e3214acee2e3182e6965da32205cbba1ef6a
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-11-08 10:53:19 +0100

    onboarding Lucene engine

---
+++ Esercizi/Lucene/Indexer.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+
+
+class Indexer {
+	public static void main(String[] args) throws IOException {
+    var analyzer = new StandardAnalyzer();
+    var path = Paths.get("text_repo");
+    var dir = FSDirectory.open(path);
+    var config = new IndexWriterConfig(analyzer);
+    var index = new IndexWriter(dir, config);
+
+    var doc1 = new Document();
+    doc1.add(new TextField("title", "Lu sule", Field.Store.YES));
+    doc1.add(new TextField(
+      "body",
+      "Il signor fratello sole, il quale è la luce del giorno, e tu tramite" + 
+      "lui ci dai la luce. E lui è bello e raggiante con grande splendore...",
+      Field.Store.YES
+    ));
+
+    var doc2 = new Document();
+    doc2.add(new TextField("title", "Lu ientu", Field.Store.YES));
+    doc2.add(new TextField(
+      "body",
+      "Lodato sii, mio Signore, per fratello vento, e per l'aria" +
+      "e per il cielo; per quello nuvoloso e per quello sereno...",
+      Field.Store.YES
+    ));
+		
+    index.addDocument(doc1);
+    index.addDocument(doc2);
+    index.close();
+	}
+}
---
+++ Esercizi/Lucene/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version=9.8.0
+wget https://dlcdn.apache.org/lucene/java/$version/lucene-$version.tgz
+tar -xf lucene-$version.tgz
+rm lucene-$version.tgz
